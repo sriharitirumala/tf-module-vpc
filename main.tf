7251dc4753f6f1d18178baf6c62db74730f3be57
sriharitirumala <sritirumala30@gmail.com>
2023-03-25 11:54:13 +0530

    [Story-1] adding correct Repos

--- main.tf
+++ main.tf
@@ -1,4 +1,20 @@
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
-  tags = merge(var.tags,{ Name = "${var.env}-vpc" })
+  tags = merge(
+    var.tags,
+    { Name = "${var.env}-vpc" }
+  )
 }
+
+## Public Subnets
+resource "aws_subnet" "public_subnets" {
+  vpc_id = aws_vpc.main.id
+  tags   = merge(
+    var.tags,
+    { Name = "${var.env}-$(each.value["name"])" }
+
+    for_each = var.public_subnets
+    cidr_block = each.value["cidr_block"]
+    availability_zone = each.value["availability_zone"]
+
+    }
