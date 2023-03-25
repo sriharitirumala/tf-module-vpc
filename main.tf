cfe565e393bc827728c2b6d6134131f399c1f8fa
sriharitirumala <sritirumala30@gmail.com>
2023-03-25 16:37:24 +0530

    [Story-1] adding correct Repos

--- main.tf
+++ main.tf
@@ -22,9 +22,25 @@ resource "aws_subnet" "public_subnets" {
 
 }
 
+## Internet gateway (IGW)
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags   = merge(
+    var.tags,
+    { Name = "${var.env}-igw" }
+  )
+}
+
+
 ## Public Route Table
 resource "aws_route_table" "public-route-table" {
   vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
 
   for_each     = var.public_subnets
   tags   = merge(
@@ -78,3 +94,12 @@ resource "aws_route_table_association" "private-association" {
   route_table_id = aws_route_table.private-route-table[each.value["name"]].id
 }
 
+## Internet gateway (IGW)
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags   = merge(
+    var.tags,
+    { Name = "${var.env}-igw" }
+  )
+}
