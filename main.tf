2e1546f48313b884017917f5d84837a8bec66e7d
sriharitirumala <sritirumala30@gmail.com>
2023-03-28 10:48:50 +0530

    adding vpc subnets, routes, route table associations

--- main.tf
+++ main.tf
@@ -132,7 +132,7 @@ resource "aws_route_table_association" "private-association" {
 
   for_each       = var.private_subnets
   subnet_id      = lookup(lookup(aws_subnet.private_subnets, each.value["name"], null), "id", null)
-  #subnet_id     = aws_subnet.private_subnets[each.value["name"]].id
+  subnet_id     = aws_subnet.private_subnets[each.value["name"]].id
   route_table_id = aws_route_table.private-route-table[each.value["name"]].id
 }
 
@@ -141,5 +141,4 @@ resource "aws_route" "route" {
   route_table_id              = var.default_route_table
   destination_cidr_block      = var.vpc_cidr
   vpc_peering_connection_id   = aws_vpc_peering_connection.peer.id
-  replace = true
 }
